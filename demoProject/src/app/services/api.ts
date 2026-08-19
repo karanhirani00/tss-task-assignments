dd7d046450c8599c8668ae4cb542432de4c9daa9
Karan Hirani <karan.hirani@tssconsultancy.com>
2026-08-19 13:54:13 +0530

    add new code

--- demoProject/src/app/services/api.ts
+++ demoProject/src/app/services/api.ts
@@ -1,13 +1,11 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable } from '@angular/core';
+import { inject, Service } from '@angular/core';
 import { Observable } from 'rxjs';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Service()
 export class Api {
 
-  constructor(private http: HttpClient) {}
+  private http = inject(HttpClient);
 
   getUsers(): Observable<any[]> {
     return this.http.get<any[]>(
